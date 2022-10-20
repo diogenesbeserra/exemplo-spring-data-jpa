6754d154fc3269e2c5cb4299182ebd925129c044
Diógenes Beserra <diogenesbeserra@gmail.com>
2022-10-19 23:48:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exemplo-spring-data-jpa
+ Curso DIO Imersão no Spring Framework com Spring Boot - Aula Spring Data JPA
---
+++ src/test/java/me/dio/aulaJPA/AulaSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.aulaJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
